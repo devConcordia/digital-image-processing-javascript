eceea0258fb55c504d2334150a24991acf7c1f7a
devConcordia <dev.rsconcordia@gmail.com>
2025-08-22 12:23:33 -0300

    feat: add FastImageEx example

--- docs/FastImageEx/main.mjs
+++ docs/FastImageEx/main.mjs
@@ -1,5 +1,5 @@
 
-import DIP from "../../../source/index.mjs";
+import DIP from "../DIP.mjs";
 
 window.addEventListener('load', function(e) {
 	
@@ -47,7 +47,7 @@ window.addEventListener('load', function(e) {
 	
 	
 	/// DIP.Load() load a image as ColorImageData
-	DIP.Load('../src/img/nuts-and-bolts.jpg', function( input, context2d ) {
+	DIP.Load('./nuts-and-bolts.jpg', function( input, context2d ) {
 		
 		///
 		divView.appendChild( preLog );
@@ -381,101 +381,5 @@ window.addEventListener('load', function(e) {
 		});
 		
 	});
-	
-	
-	
-	
-	/*
-	
-	
-	
-	
-	/// DIP.Load() load a image as ColorImageData
-	DIP.Load('../src/img/nuts-and-bolts.jpg', function( source, ctx ) {
-		
-		/// 1. save input
-		const input = source.clone();
-		
-		addStep( '1. Input', input );
-		
-		/// 2.1. grayscale filter
-		source.grayScale();
-		addStep( '2.1. GrayScale', source );
-		
-		/// 2.2. border detection with Sobel
-		source.conv( DIP.Matrix.Sobel(5) );
-		addStep( '2.2. Sobel', source );
-		
-		/// 2.3. close operation (morphology), opcional?
-	//	source.close( DIP.Matrix.Ones( 3, 3 ) );
-	//	addStep( '2.3. Morphology Close', source );
-		
-		/// 2.4. binarization (with simple thresholding)
-		source.threshold(90);
-		addStep( '2.4. Threshold', source );
-		
-		
-		/// 2.5. init Run Length Encoding Segmentation (RLESegmentation)
-		let objects = DIP.RLESegmentation.Create( source );
-		
-		/// clear source
-		source.fill( BLACK );
-		/// plot objects of RLE on source
-		objects.debug( source );
-		addStep( '2.5. Init RLESegmentation', source );
-		
-		
-		/// remove objects with area smaller than 200 pixels
-		objects.remove(function(object) { return object.pixels < 200 });
-		
-		/// clear source
-		source.fill( BLACK );
-		/// plot objects of RLE on source
-		objects.debug( source );
-		addStep( '2.6. Remove Objects with are smaller than 200 pixels', source );
-		
-		
-		/// merge object with distance (center point) smaller than 50 pixels
-		objects.merge( 50 );
-		
-		/// clear source
-		source.fill( BLACK );
-		/// plot objects of RLE on source
-		objects.debug( source );
-		addStep( '2.7. Merge objects', source );
-		
-		/// close objects of RLE (remove internal holes)
-		objects.close();
-		
-		/// clear source
-		source.fill( BLACK );
-		/// plot objects of RLE on source
-		objects.debug( source );
-		addStep( '2.8. Fill holes', source );
-
-		
-		/// create a output (blend input with current source)
-		const output = input.blend( source, 1.0, .5 );
-		
-		const RED = DIP.Color.Hex(0xff0000).getBytes();
-		
-		for( let object of objects ) {
-			
-			let { x, y, width, height } = object;
-			
-			output.setLine( x, y, x+ width, y, RED );
-			output.setLine( x+ width, y, x+ width, y+height, RED );
-			output.setLine( x+ width, y+height, x, y+height, RED );
-			output.setLine( x, y+height, x, y, RED );
-			
-		}
-		
-		addStep( '3. Output', output );
-		
-		
-	});
-	
-	
-	/**/
 	
 }, false);
